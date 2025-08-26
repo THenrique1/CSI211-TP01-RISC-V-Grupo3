5699e40be79e6dea918b19e12db9df71efe9748c
Tiago <tiago.hss@aluno.ufop.edu.br>
2025-08-26 14:15:40 -0300

    Mensagem explicativa sobre o que foi alterado

--- src/alu.v
+++ src/alu.v
@@ -1,19 +1,22 @@
-`timescale 1ns/1ps
 module ALU(
-  input  [31:0] A,
-  input  [31:0] B,
-  input  [2:0]  ALUCtrl, // 0:ADD, 1:SUB, 2:XOR, 3:SLL
-  output reg [31:0] Y,
-  output Zero
+    input [31:0] A,
+    input [31:0] B,
+    input [2:0] ALUctrl,  // 0: ADD, 1: SUB, 2: XOR, 3: SLL, 4: BNE
+    output reg [31:0] Y,
+    output Zero
 );
-  always @(*) begin
-    case (ALUCtrl)
-      3'd0: Y = A + B;
-      3'd1: Y = A - B;
-      3'd2: Y = A ^ B;
-      3'd3: Y = A << (B[4:0]);
-      default: Y = 32'b0;
+
+always @(*) begin
+    case(ALUctrl)
+        3'd0: Y = A + B;                  // ADD
+        3'd1: Y = A - B;                  // SUB
+        3'd2: Y = A ^ B;                  // XOR
+        3'd3: Y = A << (B[4:0]);          // SLL
+        3'd4: Y = (A != B) ? 32'b1 : 32'b0; // BNE
+        default: Y = 32'b0;
     endcase
-  end
-  assign Zero = (Y == 32'b0);
+end
+
+assign Zero = (Y == 32'b0); // Zero flag
+
 endmodule
